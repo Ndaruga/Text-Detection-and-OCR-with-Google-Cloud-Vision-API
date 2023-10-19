8fec8a333ceba2d1b45be0e8950bb9543430adee
Ndarugaa <ndaruga.francis@gmail.com>
2023-10-20 00:57:37 +0300

    install gcloud python in WSL

---
+++ Python/Readme.md
@@ -0,0 +1,14 @@
+# Using the Vision API with Python
+
+The Vision API allows developers to easily integrate vision detection features within applications, including image labeling, face and landmark detection, optical character recognition (OCR), and tagging of explicit content.
+
+### Prerequisites
+<ul>
+    <li>A Google Cloud project</li>
+    <ol>
+        <li>Create a <a href = "https://accounts.google.com/SignUp">google Account</a> with a .gmail extension</li>
+        <li>Sign-in to the <a href = "http://console.cloud.google.com/">Google Cloud Console</a> and create a new project or reuse an existing one</li>
+    </ol>
+    <li>A browser such as Chrome or Firefox</li>
+    <li>Basic Python</li>
+</ul>
---
+++ Python/WSL-python-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl sudo
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc
+sudo apt-get update && sudo apt-get install google-cloud-cli
+sudo apt-get install google-cloud-cli-app-engine-python
+gcloud init
+gcloud auth list
+exit
